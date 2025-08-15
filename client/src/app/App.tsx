53c526cc29eb2d8bb1b8eff8c67b6e13ea611b89
coconutick512 <bloodywargee@gmail.com>
2025-08-15 10:39:43 +0300

    a little bit client

---
+++ client/src/app/App.tsx
@@ -0,0 +1,14 @@
+import type { JSX } from 'react';
+import { useState } from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
