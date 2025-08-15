1507fd78af09dbcdf9dba57fc22e1a460d13d7f5
coconutick512 <bloodywargee@gmail.com>
2025-08-15 14:44:41 +0300

    signtabel

---
+++ client/src/entities/user/model/userSlice.ts
@@ -0,0 +1,82 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { UserState } from "../types/schema";
+import { fetchUser, loginUser, logoutUser, registerUser, scoreUser } from "./userThunk";
+
+const initialState: UserState = {
+  status: "loading",
+  user: null,
+  error: null,
+  score: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.status = "loading";
+        state.user = action.payload.user;
+        state.error = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.status = "logged";
+        state.user = null;
+        state.error = action.error.message ?? "Ошибка при входе в аккаунт";
+      });
+
+    builder
+      .addCase(logoutUser.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(logoutUser.fulfilled, (state) => {
+        state.status = "guest";
+        state.user = null;
+        state.error = null;
+      })
+      .addCase(logoutUser.rejected, (state, action) => {
+        state.status = "logged";
+        state.error = action.error.message ?? "Ошибка при выходе из аккаунта";
+      });
+
+    builder
+      .addCase(fetchUser.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        state.status = "logged";
+        state.user = action.payload.user;
+        state.error = null;
+      })
+      .addCase(fetchUser.rejected, (state, action) => {
+        state.status = "guest";
+        state.user = null;
+        state.error = action.error.message ?? "Ошибка при обновлении токена";
+      });
+
+      builder
+        .addCase(scoreUser.pending, (state) => {
+          state.status = "loading";
+          state.error = null;
+        })
+        .addCase(scoreUser.fulfilled, (state, action) => {
+          state.status = "logged";
+          state.score = action.payload.user;
+          state.error = null;
+        })
+        .addCase(scoreUser.rejected, (state, action) => {
+          state.status = "guest";
+          state.score = null;
+          state.error = action.error.message ?? "Ошибка при обновлении токена";
+        });
+  },
+});
+
+export default userSlice.reducer;
---
+++ client/src/entities/user/model/userThunk.ts
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { UserService } from "../api/userService";
+import type { UserLogin, UserRegister, UserScore } from "../types/schema";
+
+export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
+  const user = await UserService.refresh();
+  console.log(user)
+  return user;
+});
+
+export const logoutUser = createAsyncThunk("user/logoutsUser", async () => {
+  await UserService.logout();
+  return "Успешно вышли";
+});
+
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  async (formData: UserLogin) => {
+    const user = await UserService.login(formData);
+    return user;
+  }
+);
+
+export const registerUser = createAsyncThunk(
+  "user/registerUser",
+  async (formData: UserRegister) => {
+    const user = await UserService.signUp(formData);
+    return user;
+  }
+
+  
+);
+export const scoreUser = createAsyncThunk(
+  "user/scoreUser",
+  async (id: number) => {
+    const user = await UserService.findOne(id);
+    return user;
+  }
+
+  
+);
