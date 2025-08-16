a1a136af963730bd1ba28c365ca428da27c819fa
coconutick512 <bloodywargee@gmail.com>
2025-08-16 14:20:05 +0300

    Main Page

--- client/src/app/Router/Router.tsx
+++ client/src/app/Router/Router.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignPage from '@/pages/SignPage';
 import { MainLayout } from '@/app/layout/ui/MainLayout';
+import MainPage from '@/pages/MainPage';
 
 export default function Router(): React.JSX.Element {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<SignPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/sign" element={<SignPage />} />
       </Route>
     </Routes>
   );
---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,8 @@
+import React  from 'react'
+import type {JSX} from 'react'
+
+export default function MainPage(): JSX.Element {
+  return (
+    <div>Main Page</div>
+  )
+}
