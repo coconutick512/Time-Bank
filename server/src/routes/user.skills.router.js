8d6a9876cf10ced621600c4d148a3aff2398b406
coconutick512 <bloodywargee@gmail.com>
2025-08-16 15:17:59 +0300

    fix serv

--- server/src/routes/user.skills.router.js
+++ server/src/routes/user.skills.router.js
@@ -2,7 +2,7 @@ const UserSkillsController = require("../controllers/user.skills.controller");
 
 const skillRouter = require("express").Router();
 
-skillRouter.get("/:id", UserSkillsController.getUserWithSkills());
-skillRouter.get("/", UserSkillsController.getAllUsersWithSkills());
+skillRouter.get("/:id", UserSkillsController.getUserWithSkills);
+skillRouter.get("/", UserSkillsController.getAllUsersWithSkills);
 
 module.exports = skillRouter;
